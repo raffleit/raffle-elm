d8ca8f9c1fd55a3750c593a2ca9e23de09098c4a
Anders Emil Salvesen <andersems@gmail.com>
2017-09-16 22:47:21 +0200

    Initialisert

---
+++ src/Main.elm
@@ -0,0 +1,97 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+
+
+---- MODEL ----
+
+type alias Participant =
+    {
+        name: String,
+        numberOfTickets: Int
+    }
+
+type alias Model =
+    {
+        participants: List (Participant),
+        participantsForm: Participant
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    (
+    {
+        participants = [{name="hei", numberOfTickets=12}],
+        participantsForm = {name = "", numberOfTickets = 0}}
+    , Cmd.none
+    )
+
+
+
+---- UPDATE ----
+
+type Msg
+    = AddParticipant |
+    FormNameChange String |
+    FormNumberOfTicketsChange String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        AddParticipant ->
+            { model | participants = model.participantsForm :: model.participants, participantsForm = {name= "", numberOfTickets = 0} } ! [ Cmd.none ]
+
+        FormNameChange name ->
+            let
+                oldParticipantsForm = model.participantsForm
+                newParticipantsForm =
+                    { oldParticipantsForm | name = name }
+            in
+                {model | participantsForm = newParticipantsForm} ! [ Cmd.none ]
+
+        FormNumberOfTicketsChange numberOfTickets ->
+            let
+                oldParticipantsForm = model.participantsForm
+                newNumberOfTickets = Result.withDefault 0 (String.toInt numberOfTickets)
+                newParticipantsForm =
+                    { oldParticipantsForm | numberOfTickets = newNumberOfTickets }
+            in
+                {model | participantsForm = newParticipantsForm} ! [ Cmd.none ]
+
+
+---- VIEW ----
+
+viewParticipants : Participant -> Html Msg
+viewParticipants participant =
+    li [] [ text (participant.name ++ " " ++ toString participant.numberOfTickets) ]
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ img [ src "/logo.svg" ] []
+        , div [] [ text "Your Elm App is working!" ]
+        , div [] [ ul [] (List.map viewParticipants model.participants) ]
+        , div []
+              [ input [ type_ "text", value model.participantsForm.name, placeholder "Name", onInput FormNameChange ] []
+              , input [ type_ "number", value (toString model.participantsForm.numberOfTickets), placeholder "# tickets", onInput FormNumberOfTicketsChange ] []
+              , input [ type_ "submit",  onClick AddParticipant ] []
+              ]
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
