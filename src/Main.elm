e1cade41213c6a6aa0cd58c5f86b348da911dcee
Anders Emil Salvesen <andersems@gmail.com>
2017-09-17 20:45:50 +0200

    Delete works

--- src/Main.elm
+++ src/Main.elm
@@ -3,12 +3,15 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick, onSubmit)
-
+import List exposing (filter)
+import Random exposing (int, generate)
 
 ---- MODEL ----
 
+
 type alias Participant =
     {
+        id: Int,
         name: String,
         numberOfTickets: Int
     }
@@ -30,7 +33,7 @@ init : ( Model, Cmd Msg )
 init =
     (
     {
-        participants = [{name="hei", numberOfTickets=12}],
+        participants = [],
         participantsForm = {name = Nothing, numberOfTickets = Nothing}}
     , Cmd.none
     )
@@ -41,15 +44,23 @@ init =
 
 type Msg
     = AddParticipant |
+    AddParticipantWithId Int|
     FormNameChange String |
-    FormNumberOfTicketsChange String
+    FormNumberOfTicketsChange String |
+    DeleteParticipant Int
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         AddParticipant ->
-            { model | participants = {name = Maybe.withDefault "" model.participantsForm.name,
-                            numberOfTickets = (String.toInt (Maybe.withDefault "0" model.participantsForm.numberOfTickets)) |> Result.toMaybe |> Maybe.withDefault 0} :: model.participants,
+            (model, generate AddParticipantWithId (int 1000000 999999999))
+
+        AddParticipantWithId id ->
+            let
+                name = Maybe.withDefault "" model.participantsForm.name
+                numberOfTickets = (String.toInt (Maybe.withDefault "0" model.participantsForm.numberOfTickets)) |> Result.toMaybe |> Maybe.withDefault 0
+            in
+                { model | participants = { id = id, name = name, numberOfTickets = numberOfTickets} :: model.participants,
                         participantsForm = {name= Nothing, numberOfTickets = Nothing} } ! [ Cmd.none ]
 
         FormNameChange name ->
@@ -68,15 +79,49 @@ update msg model =
             in
                 {model | participantsForm = newParticipantsForm} ! [ Cmd.none ]
 
+        DeleteParticipant id ->
+            {model | participants = filter (\a -> a.id /= id) model.participants } ! [ Cmd.none ]
+
 
 ---- VIEW ----
 
-viewParticipants : Participant -> Html Msg
-viewParticipants participant =
+viewParticipant : Participant -> Html Msg
+viewParticipant participant =
     tr []
         [ td [] [text participant.name]
         , td [] [text (toString participant.numberOfTickets) ]
+        , td [] [button [classList [("btn", True), ("btn-link", True)], onClick (DeleteParticipant participant.id) ] [ text "Delete"] ]
         ]
+
+listParticipants: List (Participant) -> Html Msg
+listParticipants participants =
+    div [] [ table [ classList [("table", True)] ] [
+                thead [] [
+                    tr [] [
+                        td [] [text "Name"]
+                        , td [] [text "# tickets"]
+                        , td [] []
+                    ]
+                ]
+                , tbody [] (List.map viewParticipant participants)
+            ]
+                     ]
+
+viewParticipantForm: ParticipantsForm -> Html Msg
+viewParticipantForm form =
+    div [classList [("row", True)]]
+        [ Html.form [ onSubmit AddParticipant]
+            [ div [classList
+                [("form-group", True), ("col-md-6", True)]
+            ] [ input [ type_ "text", value (Maybe.withDefault "" form.name) , placeholder "Name", onInput FormNameChange, classList [("form-control", True)] ] [] ]
+            , div [classList
+                    [("form-group", True), ("col-md-4", True)]
+            ] [ input [ type_ "number", value (Maybe.withDefault "" form.numberOfTickets), placeholder "# tickets", onInput FormNumberOfTicketsChange, classList [("form-control", True)] ] []]
+            , div [classList
+                    [("col-md-2", True)]
+            ] [ input [ type_ "submit",  value "+", classList [("btn", True), ("btn-primary", True)] ] []]
+          ]
+      ]
 
 view : Model -> Html Msg
 view model =
@@ -89,31 +134,9 @@ view model =
             div [classList
                    [("col-md-5", True)]
             ] [
-                 div [classList
-                        [("row", True)]
-                 ] [ Html.form [ onSubmit AddParticipant]
-                        [ div [classList
-                            [("form-group", True), ("col-md-6", True)]
-                        ] [ input [ type_ "text", value (Maybe.withDefault "" model.participantsForm.name) , placeholder "Name", onInput FormNameChange, classList [("form-control", True)] ] [] ]
-                        , div [classList
-                                [("form-group", True), ("col-md-4", True)]
-                        ] [ input [ type_ "number", value (Maybe.withDefault "" model.participantsForm.numberOfTickets), placeholder "# tickets", onInput FormNumberOfTicketsChange, classList [("form-control", True)] ] []]
-                        , div [classList
-                                [("col-md-2", True)]
-                        ] [ input [ type_ "submit",  value "+", classList [("btn", True), ("btn-primary", True)] ] []]
-                      ]
-                  ]
-                , div [] [ table [ classList [("table", True)] ] [
-                                thead [] [
-                                    tr [] [
-                                        td [] [text "Name"]
-                                        , td [] [text "# tickets"]
-                                    ]
-                                ]
-                                , tbody [] (List.map viewParticipants model.participants)
-                            ]
-                 ]
-             ]
+                  (viewParticipantForm model.participantsForm)
+                , (listParticipants model.participants)
+            ]
         ]
     ]
 
